c27b372e495739d9288fb5d5c89e36fb2890f487
github username <github email address>
2019-07-17 17:36:39 +0100

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,9 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :author, :page_count
   attr_reader :title
+
+
+  GENRES = []
 
   def initialize(title)
     @title = title
@@ -8,6 +11,13 @@ class Book
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
+  end
+
+
+  # create the writer for genre and add the logic for the class constant
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre 
   end
 
 end
